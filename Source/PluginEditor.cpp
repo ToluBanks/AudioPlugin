043ce1e4c03bd459d8f115275623dde03b1965d5
ToluBanks <tbanky2000@gmail.com>
2022-04-05 22:37:17 -0400

    Implemented Skeleton for Custom Slider Visuals

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -143,7 +143,23 @@ void ResponseCurveComponent::paint(juce::Graphics& g)
 
 //==============================================================================
 SimpleEQAudioProcessorEditor::SimpleEQAudioProcessorEditor (SimpleEQAudioProcessor& p)
-    : AudioProcessorEditor (&p), audioProcessor (p), responseCurveComponent(audioProcessor), peakFreqSliderAttachment(audioProcessor.apvts, "Peak Freq", peakFreqSlider), peakGainSliderAttachment(audioProcessor.apvts, "Peak Gain", peakGainSlider), peakQualitySliderAttachment(audioProcessor.apvts, "Peak Quality", peakQualitySlider), lowCutFreqSliderAttachment(audioProcessor.apvts, "LowCut Freq", lowCutFreqSlider), highCutFreqSliderAttachment(audioProcessor.apvts, "HighCut Freq", highCutFreqSlider), lowCutSlopeSliderAttachment(audioProcessor.apvts, "LowCut Slope", lowCutSlopeSlider), highCutSlopeSliderAttachment(audioProcessor.apvts, "HighCut Slope", highCutSlopeSlider)
+    : AudioProcessorEditor (&p), audioProcessor (p), 
+    peakFreqSlider(*audioProcessor.apvts.getParameter("Peak Freq"), "Hz"), 
+    peakGainSlider(*audioProcessor.apvts.getParameter("Peak Gain"), "dB"),
+    peakQualitySlider(*audioProcessor.apvts.getParameter("Peak Qaulity"), ""), 
+    lowCutFreqSlider(*audioProcessor.apvts.getParameter("LowCut Freq"), "Hz"),
+    highCutFreqSlider(*audioProcessor.apvts.getParameter("HighCut Freq"), "Hz"),
+    lowCutSlopeSlider(*audioProcessor.apvts.getParameter("LowCut Slope"), "dB/Oct"),
+    highCutSlopeSlider(*audioProcessor.apvts.getParameter("HighCut Slope"),"dB/Oct"),
+
+    responseCurveComponent(audioProcessor),
+    peakFreqSliderAttachment(audioProcessor.apvts, "Peak Freq", peakFreqSlider),
+    peakGainSliderAttachment(audioProcessor.apvts, "Peak Gain", peakGainSlider),
+    peakQualitySliderAttachment(audioProcessor.apvts, "Peak Quality", peakQualitySlider),
+    lowCutFreqSliderAttachment(audioProcessor.apvts, "LowCut Freq", lowCutFreqSlider),
+    highCutFreqSliderAttachment(audioProcessor.apvts, "HighCut Freq", highCutFreqSlider),
+    lowCutSlopeSliderAttachment(audioProcessor.apvts, "LowCut Slope", lowCutSlopeSlider),
+    highCutSlopeSliderAttachment(audioProcessor.apvts, "HighCut Slope", highCutSlopeSlider)
     
 
 {
